af6466b81361e71137cee0a7b5f97f1e2d62a853
caojiayuan <cjy632258@hotmail.com>
2017-10-10 21:15:52 +0800

    multi version

--- start.sh
+++ start.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
-echo "eula=true" > eula.txt
+
 if [ "$JAVA_OPT" == "" ]; then
     JAVA_OPT="-Xms1G -Xmx2G"
 fi
 if [ "$VERSION" == "" ]; then
     VERSION="1.8.8"
 fi
-cp /server.properties /minecraft/server.properties
 
-FILE=version_${VERSION}.lock
-JAR=/minecraft/minecraft_server.${VERSION}.jar
+FILE=install.lock
+DIR=/minecraft/${VERSION}
+JAR=minecraft_server.${VERSION}.jar
 
+cp /server.properties ${DIR}/server.properties
+
+if [ ! -d "$DIR" ]; then
+    mkdir $DIR
+fi
+cd $DIR
+echo "eula=true" > eula.txt
 if [ ! -f "$FILE" ]; then
     echo "Download version ${VERSION}"
     rm $JAR
